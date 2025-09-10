30581178c42021c0b0f078214218aeb65a2a304b
@UrielWang <alan.uriel.perez@estudiantxs.uacm.edu.mx>
2025-09-09 18:44:21 -0600

    Sesiones.R

---
+++ Sesiones.R
@@ -0,0 +1,67 @@
+setwd("~/Metodos_UrielWang")
+25>20
+25<30
+20>=20
+20!=5
+25==25
+20+5
+20-5
+20*5
+20^5
+20%%
+10
+17%%
+17
+e = exp(1);
+x = 0.0034
+e <- exp(1)
+x <- 0.034;
+raiz<- B^2-4*B*C
+raiz<- B^2-4*A*C
+raiz<- B^2-4*A*C
+A<-5
+B<-10
+C<-8
+C<-8
+disc <-abs(raiz)
+raiz<- B^2-4*A*C
+texto<- "El valor del discriminantes es:"
+cat (texto,raiz)
+paste(texto,disc)
+paste0(texto,disc)
+X0<-1
+X1<-X0-n*X0+1
+X1<-X0-pi*X0+1
+X_1<-X1^2
+(X_1)
+X_2<-X1^2
+(X_2)
+texto1<- "El valor de"
+texto2<-"al cuadrado es:"
+paste(texto1,X1,texto2,X1_2)
+X1_2<-X1^2
+texto1<- "El valor de"
+texto2<-"al cuadrado es:"
+paste(texto1,X1,texto2,X1_2)
+ls()
+rm(list=ls())
+ls()
+ls()
+cat("Xo es", "X0", "/n","X1 es", X1)
+cat("Xo es", "X0","X1 es", X1)
+cat("X0 es","X0" ,"\n","X1 es",X1)
+cat("X0 es",X0,"\n","X1 es",X1)
+X0<-1 
+cat("Xo es", X0,"X1 es", X1)
+X1<-X0-pi*X0+1
+cat("Xo es", "X0" ,"X1 es", X1)
+cat("X0 es", "X0" ,"\n","X1 es",X1)
+a=10
+b=12
+c=11
+alfa=45
+beta=45
+gama=90
+sin_alfa<- a/c
+cos_alfa<- b/c
+tan_alfa<- a/b
